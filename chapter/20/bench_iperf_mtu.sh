98d38b8af68d2f586a41db527b86c1fc507cc3e3
Practical OPNsense <45791690+practical-opnsense@users.noreply.github.com>
2025-03-25 21:30:11 +0100

    5th edition

--- chapter/20/bench_iperf_mtu.sh
+++ chapter/20/bench_iperf_mtu.sh
@@ -6,7 +6,8 @@ foreach MTU ( 1420 1280 1024 512 256 128 )
   echo "*** Messung mit MTU ${MTU} Byte"
   ifconfig ${INTERFACE} mtu ${MTU}
   sleep 2
-  iperf3 --client 192.0.2.3 --time 60 --interval 60 --window 128K --format m | grep bit
+  iperf3 --client 192.0.2.3 --time 60 --interval 60 --omit 10 \
+    --window 128K --format m | grep bit
 end
 
 ifconfig ${INTERFACE} mtu 1500
